3831ff93c72f18269af373cf6094066e9f60acf4
Asger Rud <asgerrud@gmail.com>
2019-09-15 15:17:42 +0200

    Added beer recommender functionality

---
+++ app/constants/Beers.js
@@ -0,0 +1,9 @@
+const beerData = [
+  {name: "Sweet Bastard", fruity: 10, sweetness: 10},
+  {name: "Dark Shady", fruity: 0, sweetness: 10},
+  {name: "Ich bin ein berliner", fruity: 5, sweetness: 7 },
+  {name: "Refshale Hop", fruity: 5, sweetness: 2},
+  {name: "Bittersweet", fruity: 10, sweetness: 0 },
+]
+
+export default beerData
--- app/screens/LinksScreen.js
+++ app/screens/LinksScreen.js
@@ -1,6 +1,40 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import brain from 'brain.js';
+
+import beerData from '../constants/Beers'
+
+var trainingData = [
+  // Talisker
+  { input: { fruity: 5, sweetness: 0}, output: [1] },
+  // Highland Park
+  { input: { fruity: 7, sweetness: 0}, output: [1] },
+  // Dalwhinnie
+  { input: { fruity: 10, sweetness: 10}, output: [0] }, 
+];
+
+
+const recommendBeer = (beerList) => {
+  var percentage = 0;
+  var mostLikelyBeer;
+  
+  beerList.forEach( (beer) => {
+    a = beer.fruity
+    b = beer.sweetness
+    likeliness = Array.from(network.run({fruity: a, sweetness: b}))
+    if (likeliness > percentage) {
+      percentage = likeliness
+      mostLikelyBeer = beer
+    }
+  })
+  return mostLikelyBeer
+}
+
+const network = new brain.NeuralNetwork();
+network.train(trainingData);
+
+recommendBeer(beerData)
 
 export default function LinksScreen() {
   return (
@@ -9,13 +43,13 @@ export default function LinksScreen() {
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <ExpoLinksView />
+       <Text style={styles.padded}>{recommendBeer(beerData).name}</Text>
     </ScrollView>
   );
 }
 
 LinksScreen.navigationOptions = {
-  title: 'Links',
+  title: 'Recommendations',
 };
 
 const styles = StyleSheet.create({
@@ -24,4 +58,9 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  padded: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    fontSize: 28
+  }
 });
